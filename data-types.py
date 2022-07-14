eefcd0330e8ef3706e794d8b032fd92a783e39be
kadayogit <guledodowaa@gmail.com>
2022-07-14 11:16:34 +0300

    added data types

---
+++ data-types.py
@@ -0,0 +1,31 @@
+x = 6;  #integer type
+print(x)
+
+x = str('String type');  #string type
+print(x)
+
+x = float(5.8);  #float type
+print(x)
+
+x = complex(1j)  #complex type
+print(x)
+
+x = list(("apple", "banana", "cherry"))  #list type
+
+x = tuple(("apple", "banana", "cherry"))  #tuple type
+
+x = range(6) #range
+
+x = dict(name="John", age=36)     # dict type
+
+x = set(("apple", "banana", "cherry"))  #set type
+
+x = frozenset(("apple", "banana", "cherry"))  #frozenset type
+
+x = bool(5)	 #bool type
+
+x = bytes(5)	#bytes	type
+
+x = bytearray(5)	#bytearray type
+
+x = memoryview(bytes(5))	#memoryview type
