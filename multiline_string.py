dc1a3851e356f115c359a41805f3a1a47fbd70d3
kadayogit <guledodowaa@gmail.com>
2022-07-14 12:14:10 +0300

    added multiline string and random number

---
+++ multiline_string.py
@@ -0,0 +1,11 @@
+
+# You can use three double quotes:
+
+text = """dkjkdjfk dsjfkd k dkfj kdsjfkds lk
+ kdjfk fkldsjf lk lkdsfjkjdshf f k kjdfhjds fds"""
+print(text)
+
+# or you can use single quotes:
+text = '''dkjkdjfk dsjfkd k dkfj kdsjfkds lk
+ kdjfk fkldsjf lk lkdsfjkjdshf f k kjdfhjds fds'''
+print(text)
---
+++ random_numbers.py
@@ -0,0 +1,5 @@
+# python don't have random function, but its built-in module library, so you can import
+import random
+
+# Import the random module, and display a random number between 1 and 9:
+print(random.randrange(1, 10))
