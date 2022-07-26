e6b9687826a352cfe0bc5370dc259df8d7349407
kadayogit <guledodowaa@gmail.com>
2022-07-26 12:54:11 +0300

    Added boolean type and operators

---
+++ bitwise_operators.py
@@ -0,0 +1,8 @@
+# bitwise operators symbols
+# &, |, ^, ~, <<, >>
+
+# identity operators
+# is, is not
+
+# Memebership operators
+# in, not in
---
+++ boolean.py
@@ -0,0 +1,15 @@
+# booleans types
+print(4 > 10)
+print(4 == 8)
+
+# true condition for boolean type
+print(4 == 4)
+
+# if statement
+a = 40
+b = 33
+
+if b <  a:
+    print("B is greater than A?")
+else:
+    print("B not greater than A?")
