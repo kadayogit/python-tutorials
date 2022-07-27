3c4c55fec797cf4bbd9a52d6f0363fd72be8e250
kadayogit <guledodowaa@gmail.com>
2022-07-27 15:11:13 +0300

    Added set and dictionaries

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+# Dictionaries are used to store data values in key:value pairs.
+
+collection = {"Name": "Guled mohamed","Age": 31}
+print(collection)
+
+# Dictionaries are written with curly brackets, and have keys and values:
+
+# A dictionary can contain dictionaries, this is called nested dictionaries
+
+students = {
+"student1": {
+"name": "Ali",
+"class": "B2"
+},
+"student2": {
+    "name": "Guled",
+    "class": "B3"
+}
+}
+
+print(students)
---
+++ set.py
@@ -0,0 +1,6 @@
+# Sets are used to store multiple items in a single variable.
+# A set is a collection which is unordered, unchangeable*, and unindexed.
+
+# curly brackets 
+setlist = {'Guled Mohamed','Guled Mohamed','Kennedy wachira'}
+print(setlist)
