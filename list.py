05ef4d9ef5cf6ff5e8b8a464a165674ac9c01238
kadayogit <guledodowaa@gmail.com>
2022-07-27 14:31:22 +0300

    Added list

---
+++ list.py
@@ -0,0 +1,14 @@
+# Lists are used to store multiple items in a single variable.
+name = ['Guled', 'Raage', 'Kennedy']
+
+# list() constructor
+city = list(('Hargeisa','Burco','Berbera'))
+
+print(type(name))
+print(city)
+
+# here are four collection data types in the Python programming language:
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
