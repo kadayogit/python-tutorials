459457ad7852f40f10e81a514b0e827ba139d766
kadayogit <guledodowaa@gmail.com>
2022-07-27 15:38:36 +0300

    Added if conditions

---
+++ conditions.py
@@ -0,0 +1,11 @@
+# An "if statement" is written by using the if keyword.
+
+x = 20
+y = 2
+
+if x < y:
+    print("The x value not less then y!")
+elif x == y:
+    print("The x value greater then y!")
+else: 
+    print("The x value not less or equal to y!")
