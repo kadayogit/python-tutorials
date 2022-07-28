0f293fa8393c3702504bc29632b008573cffe872
kadayogit <guledodowaa@gmail.com>
2022-07-28 12:19:52 +0300

    Added user input function

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# User input for python
+
+username = input("Enter the name.....")
+email = input("Enter your email......")
+password = input("Enter your password..")
+
+# output all input data
+print("Your username is:" +username)
+print("Your email is:" +email)
+print("Your password is:" +password)
