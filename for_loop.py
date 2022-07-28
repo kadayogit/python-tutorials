5b808c6e4fb715ae3ad4f0f01f21ab2e9dedb77d
kadayogit <guledodowaa@gmail.com>
2022-07-28 11:15:37 +0300

    Added for loop examples

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+# A for loop is used for iterating over a sequence 
+# (that is either a list, a tuple, a dictionary, a set, or a string)
+
+city = ['Hargeisa','Burco','Borama']
+
+for i in city: 
+  print(i)
+
+
+# for number range looping
+  for y in range(6):
+   print(y)
+
+# else a for loop
+for c in range(10):
+    print("the range of c!")
+else:
+    print('Range is finished!')
--- loops.py
+++ loops.py
@@ -5,7 +5,7 @@ x = 1
 while x < 5:
  print(x)
  x +=1
- 
+
 else:
     print('x not greater 5!')
 
